7abf8a2d072bdd5d797b189cec6136d1397c87f2
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-12-03 11:01:49 -0600

    Basic metadata toggle visibility

--- js/init.js
+++ js/init.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   initMap();
+  initCustomEvents();
+  initUIEvents();
 });
+
+function initCustomEvents() {
+  
+}
+
+function initUIEvents() {
+  $('.js-metadata-btn').on('click', function() {
+    $('.metadata').animate({width: 'toggle'}, 'slow');
+  });
+}
