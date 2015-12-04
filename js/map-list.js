b3911d3c92297941aae89b25dd81f75d522e4349
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-12-04 13:21:23 -0600

    Map List formatting

--- js/map-list.js
+++ js/map-list.js
@@ -1,21 +1,29 @@
 function buildMapList() {
-  _.each(data.maps, function(v, k) {
-    var listItem = $('<li>').appendTo($('.map-list--list'))
-      
-    $('<a>')
-      .attr('href', '#')
-      .attr('data-number', parseInt(v.number))
-      .addClass('map-list--link')
-      .text(v.title.slice(0,30))
-      .appendTo(listItem)
-      .on('click', function() {
-        $(document).trigger('select:', parseInt(v.number));
-      })
-      .on('mouseover', function() {
-        $(document).trigger('highlight:', parseInt(v.number));
-      })
-      .on('mouseout', function() {
-        $(document).trigger('dehighlight:', parseInt(v.number));
-      });
+  var dateSorted = _.groupBy(data.maps, function(v, k) {
+    return +v.date;
+  });
+  
+  _.each(dateSorted, function(group) {
+    $('<h3>').text(group[0].date).appendTo($('.map-list--list'));
+    
+    _.each(group, function(v, k) {
+      var listItem = $('<li>').appendTo($('.map-list--list'))
+        
+      $('<a>')
+        .attr('href', '#')
+        .attr('data-number', +v.number)
+        .addClass('map-list--link')
+        .text(v.title)
+        .appendTo(listItem)
+        .on('click', function() {
+          $(document).trigger('select:', +v.number);
+        })
+        .on('mouseover', function() {
+          $(document).trigger('highlight:', +v.number);
+        })
+        .on('mouseout', function() {
+          $(document).trigger('dehighlight:', +v.number);
+        });
+    });
   });
 }
