d62c7139724a758d03e94bdb0beee1b49f42010c
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-12-07 17:07:04 -0600

    Map in metadata area

--- js/metadata.js
+++ js/metadata.js
@@ -1,3 +1,5 @@
+var mdmap;
+
 function buildMetadata(map) {
   if(!$('.metadata').is(':visible')) $('.metadata').show();
   
@@ -8,4 +10,36 @@ function buildMetadata(map) {
     if($('.metadata--' + k).length > 0) $('.metadata--' + k + ' .metadata--item').empty().text(map[k]);
   });
   
+  
+  if(!mdmap) {
+    mdmap = L.map('metadata--map', {
+      dragging: false,
+      touchZoom: false,
+      scrollWheelZoom: false,
+      doubleClickZoom: false,
+      boxZoom: false,
+      keyboard: false,
+      zoomControl: false,
+      attributionControl: false
+    });
+    L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png').addTo(mdmap);
+  }
+  
+  mdmap.fitBounds([[map.bottom, map.left], [map.top, map.right]]);
+  if(map.minZoom < 9) mdmap.setZoom(map.minZoom);
+  else mdmap.setZoom(9);
+  
+  mdmap.eachLayer(function(v) {
+    if(v.feature) mdmap.removeLayer(v);
+  });
+  var rect = L.rectangle([
+    [map.bottom, map.left],
+    [map.top, map.right]
+  ]).toGeoJSON();
+
+  L.geoJson(rect, {
+    style: function(f) {
+      return highlight;
+    }
+  }).addTo(mdmap);
 }
